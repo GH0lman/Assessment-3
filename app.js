b46f37d8540fca0e3638d5725d265073b4ec48dc
GH0lman <georgeholmanwork@gmail.com>
2024-10-29 17:04:40 +1300

    Part 2 - Creation of XML and Javascript

---
+++ app.js
@@ -0,0 +1,54 @@
+/* Created by George Holman - Student ID: 1580967 */
+
+// Function to parse XML data into a JavaScript object (DOM)
+function parseXML(xmlString) {
+    let parser = new DOMParser();
+    let xmlDoc = parser.parseFromString(xmlString, "application/xml");
+    return xmlDoc;
+}
+
+
+function fetchAndProcessXML() {
+    fetch('XML/areas.xml') 
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.text();
+        })
+        .then(data => {
+            let xmlData = parseXML(data);
+
+            if (xmlData.getElementsByTagName("parsererror").length > 0) {
+                console.error("Error parsing XML:", xmlData.getElementsByTagName("parsererror")[0].textContent);
+                return;
+            }
+
+            setupPopups(xmlData);
+        })
+        .catch(error => console.error("Error fetching or processing XML:", error));
+}
+
+function setupPopups(xmlData) {
+    let areas = xmlData.getElementsByTagName("area");
+
+    for (let i = 0; i < areas.length; i++) {
+        let currentArea = document.getElementById("ra-" + (i+1));
+        let currentAreaPopup = document.getElementById("popup" + (i+1));
+        let content = currentAreaPopup.getElementsByTagName("p");
+
+        content[0].innerHTML = "Cost: " + areas[i].getElementsByTagName("cost")[0].childNodes[0].nodeValue;
+        content[1].innerHTML = "Booking Status: " + areas[i].getElementsByTagName("status")[0].childNodes[0].nodeValue;
+        content[2].innerHTML = "Capacity: " + areas[i].getElementsByTagName("capacity")[0].childNodes[0].nodeValue;
+        content[3].innerHTML = "Image: " + areas[i].getElementsByTagName("src")[0].childNodes[0].nodeValue;
+        
+        if (areas[i].getElementsByTagName("status")[0].childNodes[0].nodeValue == "Reserved")
+            currentArea.style.backgroundColor = "rgba(255, 0, 0, 0.5)";
+    }
+}
+
+function revealPopup(object) {
+    object.style.visibility = "visible";
+}
+
+window.onload = fetchAndProcessXML;
